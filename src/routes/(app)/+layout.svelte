a06259aced1c420684d98cf1bc6ed352a667bb97
Oungseik <mhemaungthuwin@gmail.com>
2023-02-09 07:01:05 +0630

    feat: create header

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import profilePic from '$lib/images/profile.png';
+</script>
+
+<header class="bg-gray-800 p-4 text-gray-200 drop-shadow-md flex items-center justify-between">
+	<h1 class="font-bold font-unbounded text-2xl">Exp-T</h1>
+
+	<button class="w-9 h-9 rounded-full">
+		<img src={profilePic} alt="profile picture" />
+	</button>
+</header>
+
+<slot />
