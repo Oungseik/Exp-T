66a8b34d5e93fe0f8ded144c0a3c83cfd356afc2
Oungseik <mhemaungthuwin@gmail.com>
2023-02-12 10:17:42 +0630

    feat: setup firebase sdk

---
+++ src/lib/firebase/firebase.client.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+  PUBLIC_FIREBASE_APIKEY,
+  PUBLIC_AUTHDOMAIN,
+  PUBLIC_PROJECT_ID,
+  PUBLIC_STORAGEBUCKET,
+  PUBLIC_MESSAGING_SENDER_ID,
+  PUBLIC_APP_ID,
+  PUBLIC_MEASUREMENT_ID
+} from "$env/static/public";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: PUBLIC_FIREBASE_APIKEY,
+  authDomain: PUBLIC_AUTHDOMAIN,
+  projectId: PUBLIC_PROJECT_ID,
+  storageBucket: PUBLIC_STORAGEBUCKET,
+  messagingSenderId: PUBLIC_MESSAGING_SENDER_ID,
+  appId: PUBLIC_APP_ID,
+  measurementId: PUBLIC_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
